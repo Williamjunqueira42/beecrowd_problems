7af365875b37796f021829997bfb84a2729e2c59
William Junqueira <williamjunqueira74@gmail.com>
2026-08-15 11:36:47 -0300

    resolução do problema 1002 em C

---
+++ iniciante/1002/1002.c
@@ -0,0 +1,16 @@
+// William Junqueira
+
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+	double pi =  3.14159;
+	double raio;
+	scanf("%lf", &raio);
+	double A = pi * pow(raio, 2);
+	printf("A=%.4f\n", A);
+
+
+	return 0;
+
+}
