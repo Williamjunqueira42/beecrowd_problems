24399096856ab743c9030e3115ac4869daef9ed7
William Junqueira <williamjunqueira74@gmail.com>
2026-08-16 14:50:19 -0300

    Resolução do problema 1009 em C

---
+++ iniciante/1009/1009.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    double s, v;
+    
+    scanf("%*s");
+    scanf("%lf", &s);
+    scanf("%lf", &v);
+    printf("TOTAL = R$ %.2lf\n", s + .15*v);
+     
+       
+
+
+}
