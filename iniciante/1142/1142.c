16c66b0279156af7cad839ae9d5a306c2169b9de
William Junqueira <williamjunqueira74@gmail.com>
2026-08-25 13:35:07 -0300

    Resolução problema 1142

---
+++ iniciante/1142/1142.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+        
+    int N;
+    scanf("%d", &N);
+    for (int i = 0;i < N; i++){
+        printf("%d %d %d PUM\n", 1+i*4, 2+i*4, 3+i*4);
+    }
+    
+}    
