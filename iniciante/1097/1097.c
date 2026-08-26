11dc12d6bffbae6773d9c5deb662fa8db7830204
William Junqueira <williamjunqueira74@gmail.com>
2026-08-26 13:22:25 -0300

    Resolução do problema 1097

---
+++ iniciante/1097/1097.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int j = 7;    
+    for (int i = 1; i<=9; i+=2){
+        for (int k = j; k >= (j-2); k--){
+            printf("I=%d J=%d\n", i, k);
+        }
+        j+=2;
+    }
+    return 0;
+}
+
