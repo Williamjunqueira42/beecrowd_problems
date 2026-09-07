4ae78c49a5b330dc9e0292cc0ff9a8867d8cbacf
William Junqueira <williamjunqueira74@gmail.com>
2026-09-06 21:06:23 -0300

    resolução do problema 1151

--- iniciante/1151/1151.c
+++ iniciante/1151/1151.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
 int main() {
-        
+   
+    int N;
+    scanf("%d", &N);
+    
     int anterior = 0;
     int atual = 1;
-    int end = 0;
-    scanf("%d", &end);
-    printf("%d", anterior);
-    for (int k = 0; k < end; k++){
-        atual += anterior;
-        printf("%d ", atual);
+    int temp;
+    printf("%d %d", anterior, atual);
+    for (int k = 0; k < N-2; k++){
+        
+        printf(" %d", atual + anterior);
+        temp = anterior;
         anterior = atual;
-    
-    } 
+        atual += temp; 
+
+        
+    }
+    printf("\n");
+
+ 
+     
 }
