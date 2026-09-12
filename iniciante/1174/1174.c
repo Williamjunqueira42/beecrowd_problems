df7171e2d50db22d453fedd3a93d127472c8a2cd
William Junqueira <williamjunqueira74@gmail.com>
2026-09-11 22:52:09 -0300

    resolução problema 1174

---
+++ iniciante/1174/1174.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+        
+    double a[100];
+    for (int i = 0; i < 100; i++){
+        scanf("%lf", &a[i]);
+        if (a[i] <= 10.0){
+            printf("A[%d] = %.1lf\n", i, a[i]);
+        } 
+    }
+
+    
+}
